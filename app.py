c554e578fc75e960c0eb3d9024e4db5b8a5f4c06
unknown <you@example.com>
2021-05-27 16:58:25 +0900

    web page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask , render_template
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/', methods = ['GET','POST'])
 def hello_world():
-    return '<h1>Hello World!</h1>'
+    return render_template('home.html' , name = '김향섭')
 
 if __name__ == '__main__':
     app.run(port=8080)
